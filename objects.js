9c576656fbff289bc1adedb8221cf819381b00f1
Lydia Cho <lydiacho@gse.havard.edu>
2021-01-21 00:17:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,4 +4,5 @@ var playlist = {'Slow Dive': 'Allison'};
 var updatePlaylist = (playlist, artistName, songTitle) => {
   updatePlaylist['My Bloody Valentine'] = 'Sometimes';
   updatePlaylist['Phil Ochs'] = 'Here\'s to the State of Mississippi';
+  return updatePlaylist;
 };
