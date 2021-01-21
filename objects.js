9ef2457e3a260bd90773df44011e43e1bd2ffda6
Lydia Cho <lydiacho@gse.havard.edu>
2021-01-21 00:17:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,3 +6,5 @@ var updatePlaylist = (playlist, artistName, songTitle) => {
   updatePlaylist['Phil Ochs'] = 'Here\'s to the State of Mississippi';
   return updatePlaylist;
 };
+
+var removeFromPlaylist = ()
